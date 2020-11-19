07b41630bc5e0d74d5f27bb71e550589d38c6c2a
mohamed.samir@widebot.net <mohamed.samir@widebot.net>
2020-11-19 14:31:34 +0200

    Parallel stream

---
+++ src/stream/ParallelStream.java
@@ -0,0 +1,33 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParallelStream {
+
+	public static void main(String[] args) {
+
+		List<String> alpha = Arrays.asList("a", "b", "c", "d");
+
+        //Before Java8
+        List<String> alphaUpper = new ArrayList<>();
+        for (String s : alpha) {
+            alphaUpper.add(s.toUpperCase());
+        }
+
+        System.out.println(alpha); //[a, b, c, d]
+        System.out.println(alphaUpper); //[A, B, C, D]
+
+        // Java 8
+        List<String> collect = alpha.parallelStream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(collect); //[A, B, C, D]
+
+        // Extra, streams apply to any data type.
+        List<Integer> num = Arrays.asList(1,2,3,4,5);
+        List<Integer> collect1 = num.parallelStream().map(n -> n * 2).collect(Collectors.toList());
+        System.out.println(collect1); //[2, 4, 6, 8, 10]
+	}
+
+}
